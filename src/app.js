e9599cf2d0d7811a78deb4c4d229267e54ba020b
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-09-23 09:34:01 -0400

    get request created and send musicians as a JSON response

--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
-const { db } = require("../db/connection")
+const { Musician } = require("../models/index");
+const { db } = require("../db/connection");
 
-const port = 3000;
+const port = 3001;
 
-//TODO: Create a GET /musicians route to return all musicians 
+//TODO: Create a GET /musicians route to return all musicians
+db.sync()
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((err) => {
+    console.error("Error connecting Database", err);
+  });
 
+app.get("/musicians", async (request, response) => {
+  try {
+    const musician = await Musician.findAll();
+    response.json(musician);
+  } catch (err) {
+    console.error("Error fetching Musicians", err);
+  }
+});
 
-
-
-
-
-
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
 module.exports = app;
